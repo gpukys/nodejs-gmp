5ca3b13ef86be11542c19ed9ba0b9da3711f914b
Gerimantas Pukys <gerimantas_pukys@epam.com>
2021-03-07 14:48:45 +0200

    add babel, add line by line and all at once method

--- HT1/1.2/index.js
+++ HT1/1.2/index.js
@@ -1,17 +1,31 @@
-const csv = require('csvtojson');
-const fs = require('fs');
-const {pipeline} = require('stream');
+import csv from 'csvtojson';
+import fs from 'fs';
+import { pipeline } from 'stream';
 
-const readStream = fs.createReadStream('./data.csv');
-const writeStream = fs.createWriteStream('./result.json');
+const pathToCsv = './data.csv';
+const pathToResult = './result.json';
+const readStream = fs.createReadStream(pathToCsv);
+const writeStream = fs.createWriteStream(pathToResult);
 
-pipeline(
-  readStream,
-  csv(),
-  writeStream,
-  (error) => {
+switch (process.argv[2]) {
+  case '--all':
+    parseAll();
+  default:
+    parseLineByLine();
+}
+
+function parseLineByLine() {
+  pipeline(readStream, csv(), writeStream, (error) => {
     if (error) {
       console.error(error);
     }
-  }
-)
+  });
+}
+
+function parseAll() {
+  csv()
+    .fromFile(pathToCsv)
+    .then((jsonObj) => {
+      writeStream.write(jsonObj.toString());
+    });
+}
